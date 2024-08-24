8b330705cdc5dcb5f9ca2a18711853ef860054f7
Jasper Alblas <just_jasper@hotmail.com>
2024-08-24 22:10:57 +0200

    Adds weather fetch method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./style.css";
 
 console.log("TEST");
+
+const url =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=RT48PHEPXMEVLRTE97UTBCTR9";
+
+async function fetchWeather(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+fetchWeather(url);
