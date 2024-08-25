9e42de213793a1f43e3ecf28eb1ad10a7d13df82
Jasper Alblas <just_jasper@hotmail.com>
2024-08-25 21:27:13 +0200

    Adds functions to fetch and parse data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import "./style.css";
 
-console.log("TEST");
-
-const url =
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=RT48PHEPXMEVLRTE97UTBCTR9";
-
-async function fetchWeather(url) {
+async function fetchWeather(location) {
   try {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=RT48PHEPXMEVLRTE97UTBCTR9`;
+
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-
     const json = await response.json();
-    console.log(json);
+    return json;
   } catch (error) {
     console.error(error.message);
   }
 }
 
-fetchWeather(url);
+function parseData(json) {
+  return json.currentConditions;
+}
+
+const json = await fetchWeather("Rotterdam");
+console.log(parseData(json));
